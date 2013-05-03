31583f00eae1012db5552a4b2749517ec097d494
David Springate <daspringate@gmail.com>
2013-05-03 15:01:25 +0100

    First pass on dev server with Rook

---
+++ R/server.R
@@ -0,0 +1,27 @@
+#' Samatha static site development server
+#' 
+
+require(Rook)
+
+s <- Rhttpd$new()
+#s$start(quiet=TRUE)
+s$start("localhost:8080")
+
+## End(Not run)
+s$add(name="Samatha",
+      app=Builder$new(
+          Static$new(
+              urls = c("/css", "/img", "/js", "/resources"),
+              root = "~/github/samatha/testsite/testsite/"),
+          Brewery$new(url="/",root="~/github/samatha/testsite/testsite/"),
+          Redirect$new("/index.html")))
+
+s$add(name="test", app = File$new("~/github/samatha/testsite/testsite/"))
+
+
+
+s$browse("Samatha") # Opens a browser window to the app.
+
+s$remove(all=TRUE)
+
+
