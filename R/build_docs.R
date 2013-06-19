1213f22bbad037894d1a908f29d33b1e7b2ed2bc
David Springate <daspringate@gmail.com>
2013-06-19 16:39:33 +0100

    Documentation builder working

--- R/build_docs.R
+++ R/build_docs.R
@@ -1,7 +1,3 @@
-#' Functions for building html versions of an R package and rendering them as Static docs with Samatha layouts
-#' 
-#' 
-
 #' Converts an Rd file to an html fragment
 #' @name doc.to.html
 #' @param doc a path to an Rd file
@@ -15,9 +11,17 @@ doc.to.html <- function(doc){
                  title = name)
 }
 
-render.doc <- function(site, doc, layout, path){
+#' renders an .Rd file as a Samatha.Page object
+#' @name render.doc
+#' @param site Absolute path to the samatha site
+#' @param doc path to the .Rd document
+#' @param layout name of the layout file to render the document files with
+#' @param path relative path to the document directory in the Samatha site
+#' @param ftype character file type
+#' @return Object of class Samatha.Page
+render.doc <- function(site, doc, layout, path, ftype = "Rd_file"){
     doc.obj <- doc.to.html(doc)
-    page <- content(doc.obj$page, link.to(paste(path,"index.html", sep = "/"), "<< Back to index"))
+    page <- content(doc.obj$page)
     title <- doc.obj$title
     structure(list(html = source(file.path(site, "template/layouts", layout), local = TRUE)$value,
                                content = page,
@@ -27,10 +31,17 @@ render.doc <- function(site, doc, layout, path){
                                                 paste0(str_replace(title, "\\.", "_"), ".html")),
                                title = title,
                                sourcefile = doc,
-                               tags = "Rd_file"),
+                               tags = ftype),
                           class = "Samatha.Page")
 }
 
+#' Builds a list of Samatha.Page objects for all Rd documents in a given directory
+#' @name render.all.docs
+#' @param site character path to the Samatha site
+#' @param doc.path character path to the directory containing the Rd files
+#' @param layout name of the Samatha layout to be used to render the documents
+#' @param out.path path to documents relative to the Samatha site
+#' @return list of samatha.Page objects
 render.all.docs <- function(site, doc.path, layout, out.path){
     doc.files <- list.files(doc.path)
     lapply(doc.files,
@@ -39,15 +50,47 @@ render.all.docs <- function(site, doc.path, layout, out.path){
            })
 }
 
+#' Produces an unordered list of links to rendered documentation files
+#' @name doc.index.list
+#' @param doc.pages a list of Samatha.Page objects, as produced by render.all.docs()
+#' @param out.path path to documents relative to the Samatha site
+#' @return character string representation of an unorderd list of links to rendered documentation files
+doc.index.list <- function(doc.pages, out.path){
+    doc.names <- sapply(doc.pages, function(x) x$title)    
+    descriptions <- sapply(doc.pages, function(x) str_match(x$content, ".(<p>)([[:alnum:] , \\.]+)(</p>)")[3])
+    unordered.list(lapply(1:length(doc.names), 
+                          function(x) paste0(link.to(url = paste("",out.path, 
+                                                                  paste0(str_replace(doc.names[x], "\\.", "_"), ".html"), 
+                                                                  sep = "/"), 
+                                                      m("b", doc.names[x])), 
+                                              " ", descriptions[x])))
+}
+
+#' Renders a directory of Rd files to html and writes within a Samatha site directory
+#' @name write.docs
+#' @param site character path to the Samatha site
+#' @param doc.path path to the directory of Rd files
+#' @param layout Samatha layout file to render documentation with
+#' @param out.path path to documents relative to the Samatha site
 write.docs <- function(site, doc.path, layout, out.path){
     dir.create(file.path(site, basename(site), out.path), 
                showWarnings = FALSE, recursive = TRUE)
     message(sprintf("Rendering doc files to html and writing to %s", out.path))
     doc.pages <- render.all.docs(site, doc.path, layout, out.path)
-    
     for(pg in doc.pages){
         write.html(pg)
     }
+    page <- content(m("h1", "Package index"), doc.index.list(doc.pages, out.path))
+    index.obj <- structure(list(html = source(file.path(site, "template/layouts", layout), local = TRUE)$value,
+                   content = page,
+                   layout = layout,
+                   file = file.path(site, 
+                                    basename(site), out.path, "index.html"),
+                   title = title,
+                   sourcefile = doc,
+                   tags = "doc_index"),
+              class = "Samatha.Page")
+    write.html(index.obj)
 }
 
 
